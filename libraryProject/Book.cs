49bb8b9b049ab44a001d19438c74a4940e559987
ZhanJ02 <89538035+ZhanJ02@users.noreply.github.com>
2024-05-21 13:22:34 -0400

    Add files via upload

---
+++ libraryProject/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Milliken.Book
+{
+    public class Book
+    {
+        // Properties
+        public string Title { get; set; } = "Unknown";
+        public string Author { get; set; } = "Unknown";
+        public string Genre { get; set; } = "Fiction";
+        public int Pages { get; set; } = 1;
+        public int YearPublished { get; set; } = 1900;
+
+        // Parameterized Constructor
+        public Book(string title, string author, string genre, int pages, int yearPublished)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Pages = pages;
+            YearPublished = yearPublished;
+        }
+
+        // Print Book Details
+        public void PrintDetails()
+        {
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Pages: " + Pages);
+            Console.WriteLine("Year Published: " + YearPublished);
+        }
+    }
+}
---
+++ libraryProject/EBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Milliken.Book;
+
+namespace Milliken.EBook
+{
+    public class EBook : Book
+    {
+        // Properties
+        public double FileSize { get; set; } = 100.0;
+
+        // Parameterized Constructor
+        public EBook(string title, string author, string genre, int pages, int yearPublished, double fileSize)
+            : base(title, author, genre, pages, yearPublished)
+        {
+            FileSize = fileSize;
+        }
+
+        // Print Function
+        public void PrintEBookDetails()
+        {
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Pages: " + Pages);
+            Console.WriteLine("Year Published: " + YearPublished);
+            Console.WriteLine("File Size: " + FileSize + " MB");
+        }
+    }
+}
