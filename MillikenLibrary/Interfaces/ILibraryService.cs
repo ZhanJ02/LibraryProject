13007ca805fc3eec588ffc6620861cbf7e0f6efc
ZhanJ02 <89538035+ZhanJ02@users.noreply.github.com>
2024-06-03 15:44:42 -0400

    Add files via upload

---
+++ MillikenLibrary/Interfaces/ILibraryService.cs
@@ -0,0 +1,16 @@
+﻿using Milliken.LibrarySystem.Models;
+
+namespace Milliken.LibrarySystems.Interfaces
+{
+    public interface ILibraryService
+    {
+        // Interface for Library class
+        void AddBook(Book book);
+        void AddEBook(EBook eBook);
+        void ListAllBooks();
+        Book FindBookByTitle(string title);
+        EBook FindEBookByTitle(string title);
+        bool RemoveBooksByTitle(string title);
+        int TotalBooksAndEBooks();
+    }
+}
